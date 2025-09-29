e9c9af115ab0ffb27fdecac9f277e39f62ceac35
Jaime Breitkreutz <jaimezhb18@gmail.com>
2025-09-29 16:47:37 -0400

    mobile news fix

--- src/app/newsletter/data.ts
+++ src/app/newsletter/data.ts
@@ -1,5 +1,13 @@
 const parseDate = (dateStr) => {
-  return new Date(dateStr).getTime();
+  // Parse "Month YYYY" format more explicitly
+  const [month, year] = dateStr.split(' ');
+  const monthIndex = {
+    'January': 0, 'February': 1, 'March': 2, 'April': 3,
+    'May': 4, 'June': 5, 'July': 6, 'August': 7,
+    'September': 8, 'October': 9, 'November': 10, 'December': 11
+  }[month];
+  
+  return new Date(parseInt(year), monthIndex, 1).getTime();
 };
 
 const articles = [
