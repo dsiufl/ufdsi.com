847fa2fbab8fb43c72df813a947085b30df341d7
Jossaya Camille <jossayacamille@gmail.com>
2026-01-28 14:12:04 -0500

    reorganization

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,23 @@
+import ScrollUp from "@/components/Common/ScrollUp";
+import Features from "@/components/Features";
+import Hero from "@/components/Hero";
+import Stats from "@/components/Stats";
+import CompaniesWorked from "@/components/CompaniesWorked";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "UF DSI",
+  description: "University of Florida Data Science and Informatics Club",
+};
+
+export default function Home() {
+  return (
+    <>
+      <ScrollUp />
+      <Hero />
+      <Stats />
+      <CompaniesWorked />
+      {/* <Features /> */}
+    </>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,7 @@ import "@/styles/index.css";
 
 import { Inter } from "next/font/google";
 import { Providers } from "./providers";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+
 import ScrollToTop from "@/components/ScrollToTop";
 
 const inter = Inter({ subsets: ["latin"] });
